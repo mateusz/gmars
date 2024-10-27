954188906703af3688be5ac27b9bd3b4efd80031
Robert Lowry <bobertlo@gmail.com>
2024-10-27 18:50:12 -0500

    Load Pre-Formatted Code (#17)
    * add loader stub that parses metadata
    
    * finish 88 load file parser
    
    * add a lot of tests for 88 parser
    
    * proofread

--- pkg/mars/load.go
+++ pkg/mars/load.go
@@ -4,8 +4,40 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 )
+
+func getOpCode88(op string) (OpCode, error) {
+	switch strings.ToLower(op) {
+	case "dat":
+		return DAT, nil
+	case "mov":
+		return MOV, nil
+	case "add":
+		return ADD, nil
+	case "sub":
+		return SUB, nil
+	case "mul":
+		return MUL, nil
+	case "jmp":
+		return JMP, nil
+	case "jmz":
+		return JMZ, nil
+	case "jmn":
+		return JMN, nil
+	case "djn":
+		return DJN, nil
+	case "cmp":
+		return CMP, nil
+	case "slt":
+		return SLT, nil
+	case "spl":
+		return SPL, nil
+	default:
+		return 0, fmt.Errorf("invalid opcode '%s'", op)
+	}
+}
 
 func getOpCode(op string) (OpCode, error) {
 	switch strings.ToLower(op) {
@@ -38,8 +70,8 @@ func getOpCode(op string) (OpCode, error) {
 	}
 }
 
-func getOpMode(opMode string) (OpMode, error) {
-	switch strings.ToLower(opMode) {
+func getOpMode(opModeStr string) (OpMode, error) {
+	switch strings.ToLower(opModeStr) {
 	case "a":
 		return A, nil
 	case "b":
@@ -55,12 +87,109 @@ func getOpMode(opMode string) (OpMode, error) {
 	case "x":
 		return X, nil
 	default:
-		return 0, fmt.Errorf("invalid op mode: '%s'", opMode)
+		return 0, fmt.Errorf("invalid op mode: '%s'", opModeStr)
 	}
 }
 
-func (m *Simulator) LoadWarrior(reader io.Reader) (*Warrior, error) {
-	data := &WarriorData{
+func getAddressMode88(modeStr string) (AddressMode, error) {
+	switch modeStr {
+	case "#":
+		return IMMEDIATE, nil
+	case "$":
+		return DIRECT, nil
+	case "@":
+		return B_INDIRECT, nil
+	case "<":
+		return B_DECREMENT, nil
+	default:
+		return 0, fmt.Errorf("invalid 88 address mode: '%s'", modeStr)
+	}
+}
+
+func parseAddress(input string, coresize Address) (Address, error) {
+	val, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	m := int64(coresize)
+	val = val % m
+	if val < 0 {
+		val = (m + val) % m
+	}
+	return Address(val), nil
+}
+
+func getOpModeAndValidate88(Op OpCode, AMode AddressMode, BMode AddressMode) (OpMode, error) {
+	switch Op {
+	case DAT:
+		// DAT:
+		// F always, any combination of # and < allowed
+		if AMode != IMMEDIATE && AMode != B_DECREMENT {
+			return 0, fmt.Errorf("invalid a mode '%s' for op 'dat'", AMode)
+		}
+		if BMode != IMMEDIATE && BMode != B_DECREMENT {
+			return 0, fmt.Errorf("invalid b mode '%s' for op 'dat'", BMode)
+		}
+		return F, nil
+
+	case CMP:
+		fallthrough
+	case MOV:
+		// CMP and MOV:
+		// AB IF #A, I otherwise, no #B allowed
+		if BMode == IMMEDIATE {
+			return 0, fmt.Errorf("invalid b mode '#' for op '%s'", Op)
+		}
+		if AMode == IMMEDIATE {
+			return AB, nil
+		} else {
+			return I, nil
+		}
+
+	case SLT:
+		// SLT;
+		// AB if #A, B otherwise, no #B allowed
+		if BMode == IMMEDIATE {
+			return 0, fmt.Errorf("invalid b mode '#' for op 'slt'")
+		}
+		if AMode == IMMEDIATE {
+			return AB, nil
+		} else {
+			return B, nil
+		}
+	case ADD:
+		fallthrough
+	case SUB:
+		// ADD and SUB:
+		// AB if #A, F otherwise, no #B allowed
+		if BMode == IMMEDIATE {
+			return 0, fmt.Errorf("invalid b mode '#' for op '%s'", Op)
+		}
+		if AMode == IMMEDIATE {
+			return AB, nil
+		} else {
+			return F, nil
+		}
+	case JMP:
+		fallthrough
+	case JMN:
+		fallthrough
+	case JMZ:
+		fallthrough
+	case DJN:
+		fallthrough
+	case SPL:
+		if AMode == IMMEDIATE {
+			return 0, fmt.Errorf("invalid a mode '#' for op '%s'", Op)
+		}
+		return B, nil
+	}
+	return B, fmt.Errorf("unknown op code: '%s'", Op)
+}
+
+func parse88LoadFile(reader io.Reader, coresize Address) (WarriorData, error) {
+	data := WarriorData{
 		Name:     "Unknown",
 		Author:   "Anonymous",
 		Strategy: "",
@@ -68,50 +197,127 @@ func (m *Simulator) LoadWarrior(reader io.Reader) (*Warrior, error) {
 		Start:    0,
 	}
 
+	lineNum := 0
 	breader := bufio.NewReader(reader)
 	for {
+		// empty lines and last lines without newlines seem to be missed
+		// should something else be used? or are these not worth handling?
 		raw_line, err := breader.ReadString('\n')
 		if err != nil {
 			break
 		}
+		lineNum++
 
-		// check for these codes on the raw line
-		if strings.HasPrefix(raw_line, ";name") {
-			data.Name = strings.TrimSpace(raw_line[5:])
-			continue
-		}
-		if strings.HasPrefix(raw_line, ";author") {
-			data.Author = strings.TrimSpace(raw_line[7:])
+		if len(raw_line) == 0 {
 			continue
 		}
 
-		// clean up the line
-		line := strings.TrimSpace(raw_line)
-		if len(line) == 0 || line[0] == ';' {
+		lower := strings.ToLower(raw_line)
+
+		// handle metadata comments
+		if raw_line[0] == ';' {
+			if strings.HasPrefix(lower, ";name") {
+				data.Name = strings.TrimSpace(raw_line[5:])
+			} else if strings.HasPrefix(lower, ";author") {
+				data.Author = strings.TrimSpace(raw_line[7:])
+			} else if strings.HasPrefix(lower, ";strategy") {
+				data.Strategy += raw_line[10:]
+			}
 			continue
 		}
-		line = strings.ToLower(line)
-		line = strings.Split(line, ";")[0]
 
-		fields := strings.Fields(line)
-		if len(fields) < 2 {
-			fmt.Println("line to short")
-		} else if len(fields) == 2 {
-			if fields[0] == "org" && fields[1] == "start" {
-				continue
-			} else if fields[0] == "end" && fields[1] == "start" {
+		// trim comments
+		if strings.Contains(lower, ";") {
+			lower = strings.Split(lower, ";")[0]
+		}
+
+		// remove comma before counting fields
+		nocomma := strings.ReplaceAll(lower, ",", " ")
+
+		// split into fields based on whitespace
+		fields := strings.Fields(nocomma)
+
+		// valid instructions need exactly 5 fields
+		// only other option is "END" pseudo opcode with 0 or 1 arguments
+		if len(fields) != 5 {
+			// empty line
+			if len(fields) == 0 {
 				continue
 			}
+
+			if fields[0] != "end" {
+				return WarriorData{}, fmt.Errorf("line %d: invalid op-code '%s'", lineNum, fields[0])
+			} else if len(fields) > 2 {
+				return WarriorData{}, fmt.Errorf("line %d: too many arguments to 'end'", lineNum)
+			}
+
+			// no arguments
+			if len(fields) == 1 {
+				break
+			}
+
+			val, err := strconv.ParseInt(fields[1], 10, 32)
+			if err != nil {
+				return WarriorData{}, fmt.Errorf("line %d: error parsing integer: %s", lineNum, err)
+			}
+			if val < 0 || val > int64(len(data.Code)) {
+				return WarriorData{}, fmt.Errorf("line %d: start address outside warrior code", lineNum)
+			}
+
+			data.Start = int(val)
+			break
 		}
 
-		if len(fields) > 3 {
+		// comma is ignored, but required
+		if !strings.Contains(lower, ",") {
+			return WarriorData{}, fmt.Errorf("line %d: missing comma", lineNum)
 		}
-	}
 
-	w := &Warrior{
-		data: data,
-		sim:  m,
-	}
+		// attempt to parse the 5 fields as an instruction and append to code
+		op, err := getOpCode88(fields[0])
+		if err != nil {
+			return WarriorData{}, fmt.Errorf("line %d: %s", lineNum, err)
+		}
 
-	return w, nil
+		amode, err := getAddressMode88(fields[1])
+		if err != nil {
+			return WarriorData{}, fmt.Errorf("line %d: %s", lineNum, err)
+		}
+		aval, err := parseAddress(fields[2], coresize)
+		if err != nil {
+			return WarriorData{}, fmt.Errorf("line %d: error parsing a field integer: %s", lineNum, err)
+		}
+
+		bmode, err := getAddressMode88(fields[3])
+		if err != nil {
+			return WarriorData{}, fmt.Errorf("line %d: %s", lineNum, err)
+		}
+		bval, err := parseAddress(fields[4], coresize)
+		if err != nil {
+			return WarriorData{}, fmt.Errorf("line %d: error parsing b field integer: %s", lineNum, err)
+		}
+
+		opmode, err := getOpModeAndValidate88(op, amode, bmode)
+		if err != nil {
+			return WarriorData{}, fmt.Errorf("line %d: %s", lineNum, err)
+		}
+
+		data.Code = append(data.Code, Instruction{
+			Op:     op,
+			OpMode: opmode,
+			AMode:  amode,
+			A:      aval,
+			BMode:  bmode,
+			B:      bval,
+		})
+
+	}
+	return data, nil
+}
+
+func ParseLoadFile(reader io.Reader, simConfig SimulatorConfig) (WarriorData, error) {
+	if simConfig.Mode != ICWS88 {
+		return WarriorData{}, fmt.Errorf("not implemented")
+	}
+	return parse88LoadFile(reader, simConfig.CoreSize)
 }
---
+++ pkg/mars/load_test.go
@@ -0,0 +1,200 @@
+package mars
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type instructionParseTestCase struct {
+	input  string
+	output Instruction
+}
+
+const imp88 = `;redcode
+;name Imp
+;author A K Dewdney
+;strategy this is the simplest program
+;strategy it was described in the initial articles
+
+		MOV $ 0, $ 1
+		END 0
+`
+
+func TestLoadImp(t *testing.T) {
+	config := StandardConfig()
+
+	reader := strings.NewReader(imp88)
+	data, err := ParseLoadFile(reader, config)
+	require.NoError(t, err)
+	require.Equal(t, "Imp", data.Name)
+	require.Equal(t, "A K Dewdney", data.Author)
+	require.Equal(t, "this is the simplest program\nit was described in the initial articles\n", data.Strategy)
+	require.Equal(t, 0, data.Start)
+	require.Equal(t, 1, len(data.Code))
+	require.Equal(t, Instruction{Op: MOV, OpMode: I, AMode: DIRECT, A: 0, BMode: DIRECT, B: 1}, data.Code[0])
+}
+
+func TestLoadDwarf(t *testing.T) {
+	config := StandardConfig()
+
+	dwarf_code := `
+	ADD #  4,  $  3
+	MOV $  2,  @  2
+	JMP $ -2,  $  0
+	DAT #  0,  #  0
+	`
+
+	reader := strings.NewReader(dwarf_code)
+	data, err := ParseLoadFile(reader, config)
+	require.NoError(t, err)
+	require.Equal(t, 0, data.Start)
+	require.Equal(t, 4, len(data.Code))
+	require.Equal(t, []Instruction{
+		{Op: ADD, OpMode: AB, AMode: IMMEDIATE, A: 4, BMode: DIRECT, B: 3},
+		{Op: MOV, OpMode: I, AMode: DIRECT, A: 2, BMode: B_INDIRECT, B: 2},
+		{Op: JMP, OpMode: B, AMode: DIRECT, A: 8000 - 2, BMode: DIRECT, B: 0},
+		{Op: DAT, OpMode: F, AMode: IMMEDIATE, A: 0, BMode: IMMEDIATE, B: 0},
+	}, data.Code)
+}
+
+func TestValidInput(t *testing.T) {
+	// random inputs that are valid but not worth validating output
+	cases := []string{
+		"END\n",
+		"\n\n",
+	}
+
+	config := StandardConfig()
+	for i, testCase := range cases {
+		reader := strings.NewReader(testCase)
+		_, err := ParseLoadFile(reader, config)
+		assert.NoError(t, err, "test: %d' '%s'", i, testCase)
+	}
+}
+
+func TestInvalidInput(t *testing.T) {
+	// random inputs that will throw an error
+	cases := []string{
+		// random invalid token combinations
+		"END invalid ;\n",
+		"END 1 2\n",
+		"OTHER ; bad short op code\n",
+		"CMP $0, $ 0 ; missing space\n",
+		"CMP $ 0, $0 ; missing space\n",
+		"CMP $ 0 $ 0 ; no comma\n",
+		"INV $ 0, $ 0 ; invalid opcode\n",
+		// bad op address modes
+		"DAT $ 0, # 0\n",
+		"DAT @ 0, # 0\n",
+		"DAT # 0, $ 0\n",
+		"DAT # 0, @ 0\n",
+		"CMP $ 0, # 0\n",
+		"SLT $ 0, # 0\n",
+		"ADD $ 0, # 0\n",
+		"SUB $ 0, # 0\n",
+		"JMP # 0, $ 0\n",
+		"JMN # 0, $ 0\n",
+		"JMZ # 0, $ 0\n",
+		"DJN # 0, $ 0\n",
+		"SPL # 0, $ 0\n",
+		"MOV $ 0, $ 1\nEND 2 ; BAD END ADDRESS\n",
+		"MOV $ 0, $ 1\nEND -2 ; BAD END ADDRESS\n",
+		// invalid addresses and modes
+		"MOV ! 0, $ 1\n",
+		"MOV $ 0, ! 1\n",
+		"MOV $ oops, $ 1\n",
+		"MOV $ 0, $ ooops\n",
+	}
+
+	config := StandardConfig()
+
+	for i, testCase := range cases {
+		reader := strings.NewReader(testCase)
+		out, err := ParseLoadFile(reader, config)
+		assert.Error(t, err, fmt.Sprintf("test %d: '%s'", i, testCase))
+		assert.Equal(t, 0, len(out.Code))
+	}
+}
+
+func TestValidOpModeCombos88(t *testing.T) {
+	// these are all valid op mode combinations
+	// any op not included falls through to one of these
+
+	testCases := []instructionParseTestCase{
+		// DAT is special!
+		{"DAT # 1, # 2\n", Instruction{Op: DAT, OpMode: F, AMode: IMMEDIATE, A: 1, BMode: IMMEDIATE, B: 2}},
+		{"DAT # 1, < 2\n", Instruction{Op: DAT, OpMode: F, AMode: IMMEDIATE, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"DAT < 1, # 2\n", Instruction{Op: DAT, OpMode: F, AMode: B_DECREMENT, A: 1, BMode: IMMEDIATE, B: 2}},
+		{"DAT < 1, < 2\n", Instruction{Op: DAT, OpMode: F, AMode: B_DECREMENT, A: 1, BMode: B_DECREMENT, B: 2}},
+
+		// CMP, MOV
+		{"MOV # 1, $ 2\n", Instruction{Op: MOV, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: DIRECT, B: 2}},
+		{"MOV # 1, @ 2\n", Instruction{Op: MOV, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"MOV # 1, < 2\n", Instruction{Op: MOV, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"MOV $ 1, $ 2\n", Instruction{Op: MOV, OpMode: I, AMode: DIRECT, A: 1, BMode: DIRECT, B: 2}},
+		{"MOV $ 1, @ 2\n", Instruction{Op: MOV, OpMode: I, AMode: DIRECT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"MOV $ 1, < 2\n", Instruction{Op: MOV, OpMode: I, AMode: DIRECT, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"MOV @ 1, $ 2\n", Instruction{Op: MOV, OpMode: I, AMode: B_INDIRECT, A: 1, BMode: DIRECT, B: 2}},
+		{"MOV @ 1, @ 2\n", Instruction{Op: MOV, OpMode: I, AMode: B_INDIRECT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"MOV @ 1, < 2\n", Instruction{Op: MOV, OpMode: I, AMode: B_INDIRECT, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"MOV < 1, $ 2\n", Instruction{Op: MOV, OpMode: I, AMode: B_DECREMENT, A: 1, BMode: DIRECT, B: 2}},
+		{"MOV < 1, @ 2\n", Instruction{Op: MOV, OpMode: I, AMode: B_DECREMENT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"MOV < 1, < 2\n", Instruction{Op: MOV, OpMode: I, AMode: B_DECREMENT, A: 1, BMode: B_DECREMENT, B: 2}},
+
+		// ADD, SUB
+		{"ADD # 1, $ 2\n", Instruction{Op: ADD, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: DIRECT, B: 2}},
+		{"ADD # 1, @ 2\n", Instruction{Op: ADD, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"ADD # 1, < 2\n", Instruction{Op: ADD, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"ADD $ 1, $ 2\n", Instruction{Op: ADD, OpMode: F, AMode: DIRECT, A: 1, BMode: DIRECT, B: 2}},
+		{"ADD $ 1, @ 2\n", Instruction{Op: ADD, OpMode: F, AMode: DIRECT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"ADD $ 1, < 2\n", Instruction{Op: ADD, OpMode: F, AMode: DIRECT, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"ADD @ 1, $ 2\n", Instruction{Op: ADD, OpMode: F, AMode: B_INDIRECT, A: 1, BMode: DIRECT, B: 2}},
+		{"ADD @ 1, @ 2\n", Instruction{Op: ADD, OpMode: F, AMode: B_INDIRECT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"ADD @ 1, < 2\n", Instruction{Op: ADD, OpMode: F, AMode: B_INDIRECT, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"ADD < 1, $ 2\n", Instruction{Op: ADD, OpMode: F, AMode: B_DECREMENT, A: 1, BMode: DIRECT, B: 2}},
+		{"ADD < 1, @ 2\n", Instruction{Op: ADD, OpMode: F, AMode: B_DECREMENT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"ADD < 1, < 2\n", Instruction{Op: ADD, OpMode: F, AMode: B_DECREMENT, A: 1, BMode: B_DECREMENT, B: 2}},
+
+		// Just SLT
+		{"SLT # 1, $ 2\n", Instruction{Op: SLT, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: DIRECT, B: 2}},
+		{"SLT # 1, @ 2\n", Instruction{Op: SLT, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"SLT # 1, < 2\n", Instruction{Op: SLT, OpMode: AB, AMode: IMMEDIATE, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"SLT $ 1, $ 2\n", Instruction{Op: SLT, OpMode: B, AMode: DIRECT, A: 1, BMode: DIRECT, B: 2}},
+		{"SLT $ 1, @ 2\n", Instruction{Op: SLT, OpMode: B, AMode: DIRECT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"SLT $ 1, < 2\n", Instruction{Op: SLT, OpMode: B, AMode: DIRECT, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"SLT @ 1, $ 2\n", Instruction{Op: SLT, OpMode: B, AMode: B_INDIRECT, A: 1, BMode: DIRECT, B: 2}},
+		{"SLT @ 1, @ 2\n", Instruction{Op: SLT, OpMode: B, AMode: B_INDIRECT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"SLT @ 1, < 2\n", Instruction{Op: SLT, OpMode: B, AMode: B_INDIRECT, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"SLT < 1, $ 2\n", Instruction{Op: SLT, OpMode: B, AMode: B_DECREMENT, A: 1, BMode: DIRECT, B: 2}},
+		{"SLT < 1, @ 2\n", Instruction{Op: SLT, OpMode: B, AMode: B_DECREMENT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"SLT < 1, < 2\n", Instruction{Op: SLT, OpMode: B, AMode: B_DECREMENT, A: 1, BMode: B_DECREMENT, B: 2}},
+
+		// JMP, JMN, JMZ, DJN, SPL
+		{"JMP $ 1, # 2\n", Instruction{Op: JMP, OpMode: B, AMode: DIRECT, A: 1, BMode: IMMEDIATE, B: 2}},
+		{"JMP $ 1, $ 2\n", Instruction{Op: JMP, OpMode: B, AMode: DIRECT, A: 1, BMode: DIRECT, B: 2}},
+		{"JMP $ 1, @ 2\n", Instruction{Op: JMP, OpMode: B, AMode: DIRECT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"JMP $ 1, < 2\n", Instruction{Op: JMP, OpMode: B, AMode: DIRECT, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"JMP @ 1, # 2\n", Instruction{Op: JMP, OpMode: B, AMode: B_INDIRECT, A: 1, BMode: IMMEDIATE, B: 2}},
+		{"JMP @ 1, $ 2\n", Instruction{Op: JMP, OpMode: B, AMode: B_INDIRECT, A: 1, BMode: DIRECT, B: 2}},
+		{"JMP @ 1, @ 2\n", Instruction{Op: JMP, OpMode: B, AMode: B_INDIRECT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"JMP @ 1, < 2\n", Instruction{Op: JMP, OpMode: B, AMode: B_INDIRECT, A: 1, BMode: B_DECREMENT, B: 2}},
+		{"JMP < 1, # 2\n", Instruction{Op: JMP, OpMode: B, AMode: B_DECREMENT, A: 1, BMode: IMMEDIATE, B: 2}},
+		{"JMP < 1, $ 2\n", Instruction{Op: JMP, OpMode: B, AMode: B_DECREMENT, A: 1, BMode: DIRECT, B: 2}},
+		{"JMP < 1, @ 2\n", Instruction{Op: JMP, OpMode: B, AMode: B_DECREMENT, A: 1, BMode: B_INDIRECT, B: 2}},
+		{"JMP < 1, < 2\n", Instruction{Op: JMP, OpMode: B, AMode: B_DECREMENT, A: 1, BMode: B_DECREMENT, B: 2}},
+	}
+
+	config := StandardConfig()
+
+	for i, testCase := range testCases {
+		reader := strings.NewReader(testCase.input)
+		out, err := ParseLoadFile(reader, config)
+		require.NoError(t, err, fmt.Sprintf("test %d: parsing '%s' failed: '%s", i, testCase.input, err))
+		require.Equal(t, 1, len(out.Code), fmt.Sprintf("test %d: '%s'", i, testCase.input))
+		assert.Equal(t, testCase.output, out.Code[0], fmt.Sprintf("test %d: '%s'", i, testCase.input))
+	}
+}
